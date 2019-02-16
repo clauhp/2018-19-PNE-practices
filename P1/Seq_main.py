6f4f4ec3f3bbba7773eec70915f5466ea74b2094
clauhp <claudia8838@homail.es>
2019-02-16 11:05:59 +0100

    Practice 1 main program

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -4,17 +4,19 @@ from Seq import Seq
 
 s1 = Seq(input("Please introduce a first sequence: ").upper())
 s2 = Seq(input("Please, introduce a second sequence: ").upper())
-s3 = s1.complement()
-s4 = s3.reverse()
+s3 = Seq(s1.complement())
+s4 = Seq(s3.reverse())
+
+
 
 sequences = s1, s2, s3, s4
 n = 1
+bases = ["A","C","G","T"]
+
 
 for s in sequences:
-    print("Sequence {}: {}".format(n,s1), "\n   Length: {}".format(s.len()))
-
-
-
-
-
-
+    print("\nSequence {}: {}".format(n,s.strbases), "\n   Length: {}".format(s.len()), "\n   Bases count: ", end='')
+    d_count = s.count()
+    print('A:{},'.format(d_count['A']), 'C:{},'.format(d_count['C']), 'G:{},'.format(d_count['G']), 'T:{}'.format(d_count['T']), '\n   Bases percentage: ', end='')
+    print('A:{}%,'.format(s.perc('A')), 'C:{}%,'.format(s.perc('C')), 'G:{}%,'.format(s.perc('G')), 'T:{}%'.format(s.perc('T')))
+    n += 1
