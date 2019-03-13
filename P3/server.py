099da5021b0d70fe0124ef37da8cc57d7f4cf89a
clauhp <claudia8838@homail.es>
2019-03-13 18:24:19 +0100

    Practice 3 Not finished yet
    Signed-off-by: clauhp <claudia8838@homail.es>

--- P3/server.py
+++ P3/server.py
@@ -1,21 +1,49 @@
 import socket
+from P3.Seq_P3 import Seq
 
-IP = "192.168.1.39"
-PORT = 8080
+
+IP = "10.3.50.176"
+PORT = 8082
 MAX_OPEN_REQUEST = 5
 
 # IT IS NOT FINISHED YET!!!
 
+
 def client_request(cr):
     # Reading the message from the client
-    msg = cr.recv(2048).decode("utf-8")
+    msg = cr.recv(2048).decode('utf-8')
 
     if msg == '':
-        answer = 'ALIVE'
-        cr.send(str.encode(answer))
-    else:
-        pass
+        cr.send(str.encode('ALIVE'))
+        cr.close()
 
+    else:
+        msg = msg.split('\n')
+        dnaseq = msg[0]
+        seq = Seq(dnaseq)
+
+        bases = ['A', 'C', 'T', 'G']
+
+        for c in dnaseq:
+            if c not in bases:
+                cr.send(str.encode("ERROR"))
+                cr.close()
+
+        to_do = {"len": seq.len(), "complement": seq.complement(), "reverse": seq.reverse()}
+
+        for i in msg[1:]:
+            if "count" in i:
+                msg_out = seq.count()
+                base = i[-1]
+                return msg_out[base]
+            elif "perc" in i:
+                base = i[-1]
+                return seq.perc(base)
+            elif i in to_do:
+                msg_out = to_do[i]
+                return msg_out
+            else:
+                pass
 
 
 s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,11 +53,11 @@ s_socket.bind((IP, PORT))
 s_socket.listen(MAX_OPEN_REQUEST)
 
 while True:
-    c_socket, address = s_socket.accept()
+    (c_socket, address) = s_socket.accept()
 
-    #-- Process the client request
+    # -- Process the client request
     print("Attending client: {}".format(address))
 
-    client_request(s_socket)
+    client_request(c_socket)
 
-    #FGHJKLLIU
+    c_socket.close()
