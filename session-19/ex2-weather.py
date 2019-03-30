98cd2427f96d35916035c22aba768e3fca3dce31
clauhp <claudia8838@homail.es>
2019-03-30 20:23:04 +0100

    session 19 ex 2 finished
    Signed-off-by: clauhp <claudia8838@homail.es>

--- session-19/ex2-weather.py
+++ session-19/ex2-weather.py
@@ -5,22 +5,23 @@ import json
 
 
 def retrievewoeid(place):
-    HOSTNAME = "www.metaweather.com"
 
-    ENDPOINT = "/api/location/search/?query="
-    ENDPOINT += place
-    METHOD = "GET"
+    HOSTNAME1 = "www.metaweather.com"
+
+    ENDPOINT1 = "/api/location/search/?query="
+    ENDPOINT1 += place
+    METHOD1 = "GET"
 
     # -- Here we can define special headers if needed
     headers = {'User-Agent': 'http-client'}
 
     # -- Connect to the server
     # -- NOTICE it is an HTTPS connection!
-    conn = http.client.HTTPSConnection(HOSTNAME)
+    conn = http.client.HTTPSConnection(HOSTNAME1)
 
     # -- Send the request. No body (None)
     # -- Use the defined headers
-    conn.request(METHOD, ENDPOINT, None, headers)
+    conn.request(METHOD1, ENDPOINT1, None, headers)
 
     # -- Wait for the server's response
     r1 = conn.getresponse()
@@ -36,35 +37,36 @@ def retrievewoeid(place):
 
 HOSTNAME = "www.metaweather.com"
 citydata = retrievewoeid(input('Please introduce a capital of the world: '))
-woeid = citydata[0]['woeid']
-ENDPOINT = "/api/location/" + str(woeid) +'/'
-METHOD = 'GET'
-
-headers = {'User-Agent': 'http-client'}
-
-conn = http.client.HTTPSConnection(HOSTNAME)
-
-conn.request(METHOD, ENDPOINT, None, headers)
-
-r1 = conn.getresponse()
-
-text_json = r1.read().decode("utf-8")
-conn.close()
-
-weather = json.loads(text_json)
 
 
-time = weather['time']
-temp0 = weather['consolidated_weather'][0]
-temp = temp0['the_temp']
+try:
 
-print()
-print("Time: {}".format(time))
-print("Current temp: {} degrees".format(temp))
+    woeid = citydata[0]['woeid']
+    ENDPOINT = "/api/location/" + str(woeid) + '/'
+    METHOD = 'GET'
 
+    headers = {'User-Agent': 'http-client'}
 
+    conn = http.client.HTTPSConnection(HOSTNAME)
 
+    conn.request(METHOD, ENDPOINT, None, headers)
 
+    r1 = conn.getresponse()
 
-# -- Read the response's body and close
-# -- the connection
+    text_json = r1.read().decode("utf-8")
+    conn.close()
+
+    weather = json.loads(text_json)
+
+    time = weather['time'].split('T')[1].split('.')
+    temp0 = weather['consolidated_weather'][0]
+    temp = temp0['the_temp']
+    sunset = weather['sun_set'].split('T')[1].split('.')
+
+    print()
+    print("Time: {}".format(time[0]))
+    print("Temperature: {} ºC".format(temp))
+    print('Sun set time: {}'.format(sunset[0]))
+
+except IndexError:
+    print("Sorry, there's no info about that city")
