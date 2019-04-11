a71e3bbca5916697cec294ae1f496dfc3d64ced7
clauhp <claudia8838@homail.es>
2019-04-11 19:09:24 +0200

    Practice 6 html error
    Signed-off-by: clauhp <claudia8838@homail.es>

--- P6/server.py
+++ P6/server.py
@@ -4,7 +4,7 @@ import termcolor
 from P1.Seq import Seq
 
 
-PORT = 8000
+PORT = 8089
 
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
@@ -15,54 +15,77 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         termcolor.cprint(self.path, 'blue')
         contents = ''
         info = {}
+        path = self.path
 
-
-        if self.path == '/':
+        if path == '/':
 
             f = open("mainform.html", 'r')
             contents = f.read()
 
-        elif self.path.startswith('/Seq') and '=' in self.path:
+        elif path.startswith('/Seq' or '/') and '=' in path:
             f = open('response.html', 'w')
-            things = self.path.split('&')
+            things = path.split('&')
             msg = things[0]
             dna = msg[9:]
             base = things[-2].split('=')[1]
             s = Seq(dna.upper())
             operation = things[-1].split('=')[1]
 
-            for x in things:
-                if 'count' in x:
-                    number = s.count()
-                    info.update({operation: number[base]})
-                    print(number)
+            if not all(letter in "ACTGactg" for letter in dna):
+                content = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Sequence</title>
+</head>
+<body>
+  <h1>Sequence analysis</h1>
+  <p>Sorry, the DNA sequence is not valid. Please try again.</p>
+  <a href="/">Main page</a>
+</body>
+</html>"""
+                f.write(content)
+                f.close()
+                f = open('response.html', 'r')
+                contents = f.read()
 
-                elif 'perc' in x:
-                    percentage = s.perc(base)
-                    info.update({operation: percentage})
+            else:
 
-                elif 'chk=on' in x:
-                    info.update({'length': s.len()})
+                for x in things:
+                    if 'count' in x:
+                        number = s.count()
+                        info.update({operation: number[base]})
 
-            content = """<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Sequence</title>
-    </head>
-    <body>
-      <h1>Sequence analysis</h1>
-      <p>Sequence: {}</p>
-      <p></p>
-      <p>{}</p>
-      <p></p>
-      <p>Operation {} on the {} base: {}</p>
-      <a href="/">Main page</a>
-    </body>
-  </html>""".format(dna, info.get('length'), operation, base, info.get(operation))
-            f.write(content)
-            f.close()
-            f = open('response.html', 'r')
+                    elif 'perc' in x:
+                        percentage = s.perc(base)
+                        info.update({operation: percentage})
+
+                    elif 'chk=on' in x:
+                        info.update({'length': s.len()})
+
+                content = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Sequence</title>
+</head>
+<body>
+  <h1>Sequence analysis</h1>
+  <p>Sequence: {}</p>
+  <p></p>
+  <p>{}</p>
+  <p></p>
+  <p>Operation {} on the {} base: {}</p>
+  <a href="/">Main page</a>
+</body>
+</html>""".format(dna.upper(), info.get('length'), operation, base, info.get(operation))
+                f.write(content)
+                f.close()
+                f = open('response.html', 'r')
+                contents = f.read()
+
+        else:
+            f = open("error.html", "r")
             contents = f.read()
 
         self.send_response(200)
