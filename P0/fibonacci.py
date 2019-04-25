b3a9dc98defc074cc59693db300a0fca9dd2cbda
clauhp <claudia8838@homail.es>
2019-04-25 10:06:33 +0200

    Practice 0 improved
    Signed-off-by: clauhp <claudia8838@homail.es>

--- P0/fibonacci.py
+++ P0/fibonacci.py
@@ -1,20 +1,8 @@
-<<<<<<< Updated upstream
-def fibonacci_sr(n):
-    a = 0
-    b = 1
-    num = n
-    for i in range(num):
-        x = print(a, end=' , ')
-        nth = a + b
-        a = b
-        b = nth
-    return x
-serie = fibonacci_sr (int(input("Please, inserte the number you want: ")))
-print (serie)
-=======
-n = int(input("Please, introduce a number: "))
-serie = 0
-for num in range(n):
-    serie = num + n[num-1]
-    print (serie)
->>>>>>> Stashed changes
+num = int(input("Please, inserte the number you want: "))
+a, b = 0, 1
+
+for i in range(num):
+    print(a, end=' ')
+    nth = a + b
+    a = b
+    b = nth
